0df06cb513b56f9b18b6d5457af2a694b4f9a6fa
narayaniitk23-27 <143210066+narayaniitk23-27@users.noreply.github.com>
2025-06-27 16:37:22 +0530

    Add files via upload

---
+++ frontend/src/pages/Gallery.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HostelGallery from "../components/HostelGallery";
+
+const Gallery = () => {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Hostel Gallery</h2>
+      <HostelGallery />
+    </div>
+  );
+};
+
+export default Gallery;
